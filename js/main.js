575c4f9f93d6b5116f61f30160e8d975544ef6d1
Aaron Morris <morrisai@gmail.com>
2018-05-10 21:59:45 -0400

    updated for 2018

--- js/main.js
+++ js/main.js
@@ -1,40 +1,26 @@
-function parseDateRange(string) {
-	var vals = string.split(' - ');
-	return moment(vals[0], 'MM DD YYYY').twix(vals[1], 'MM DD YYYY');
-}
-
-function getDateRanges() {
-	return $('.date-range').map(function (i, inputEl) {
-		return parseDateRange($(inputEl).val());
-	}).get();
-}
-
 $( document ).ready( function(){
 
-		var cppMaxEarning = 55300;
+		var cppMaxEarning = 52500;
 		var cppExemptAmount = 3500;
-		var eiMaxEarning = 51300.00;
+		var eiMaxEarning = 48600;
 		var cppPerc =  0.0495;
-		var eiPerc = 0.0163;
+		var eiPerc = 0.0192;
 
-		var year = 2017;
-		var jan1 = new Date(year, 00, 01);
+		var jan1 = new Date(2014, 00, 01);
 		var day  = moment(new Date (date('16 weeks',jan1))).format("MMMM Do") ;
 
 		var payPeriods = [4, 2, 2, 1];
 
+		
+	
 
 		$("#submitButton").click(function(e){
 			e.preventDefault();
 
-			if (!validateDateRanges()){
-				return;
-			}
-
-			var ytd =  parseInt($("#ytdInput").val().replace(/[^0-9.]/g, ''));
+			var ytd =  parseInt($("#ytdInput").val().replace(/,/g, ''));
 
 
-			var compensation = parseInt($("#compensationInput").val().replace(/[^0-9.]/g, ''));
+			var compensation = parseInt($("#compensationInput").val().replace(/,/g, ''));
 
 			//console.log(compensation);
 			if(isNaN(compensation)){
@@ -55,31 +41,11 @@ $( document ).ready( function(){
 			var weeksOfPayCPP = Math.round(resultCPP*payPeriods[$("#numPaysSelect")[0].selectedIndex]);
 			var weeksOfPayEI = Math.round(resultEI*payPeriods[$("#numPaysSelect")[0].selectedIndex]);
 
-			var ranges = getDateRanges();
+			
 
-			function payoffDateReducer (acc, range, i, arr) {
-				if (acc.payoffDate) {
-					return acc;
-				} else if (acc.weeksRemaining <= range.length('weeks')) {
-					return {
-						weeksRemaining: 0,
-						payoffDate: range.start().add(acc.weeksRemaining, 'weeks')
-					};
-				} else {
-					return {
-						weeksRemaining: acc.weeksRemaining - range.length('weeks'),
-						payoffDate: undefined
-					};
-				}
-			}
+			var dayCpp  = moment(new Date (date( weeksOfPayCPP+' weeks',jan1))).format("MMMM Do") ;
 
-			var dayCpp = ranges
-				.reduce(payoffDateReducer, {weeksRemaining: weeksOfPayCPP, payoffDate: undefined})
-				.payoffDate;
-
-			var dayEI = ranges
-				.reduce(payoffDateReducer, {weeksRemaining: weeksOfPayEI, payoffDate: undefined})
-				.payoffDate;
+			var dayEI  = moment(new Date (date( weeksOfPayEI+' weeks',jan1))).format("MMMM Do") ;
 
 			$("#results").removeClass("alert alert-danger");
 
@@ -89,104 +55,27 @@ $( document ).ready( function(){
 
 					$("#results").addClass("alert alert-danger");
 			} else {
-				var cppPay= Math.round(amountPerPay*cppPerc);
-				var eiPay =  Math.round(amountPerPay*eiPerc);
-
-				var payDayCpp = Math.ceil(resultCPP);
-				var payDayEi = Math.ceil(resultEI);
-
-				var totalExtra = cppPay + eiPay;
-				var totalExtraInYear = Math.round(( numPays -  ( (resultCPP+resultEI) / 2 ) )*  totalExtra);
-
-				var response = "Your CPP contribution is <b>$" + cppPay + "</b> per pay and your EI contribution is <b>$" +eiPay + "</b>. " + 
-
-				"<br/><br/>You should max out your CPP Contributions on your " + nth(payDayCpp)+ " pay. "+
-				"<br/>You should max out your EI Contributions on your " + nth(payDayEi)+ " pay. ";
-
-				if (dayCpp && dayEI) {
-					response += "<br/><br/>Very very roughly this means " +dayCpp.format("MMMM Do") + " for CPP and " + dayEI.format("MMMM Do")+ " for EI. <br /> <br /> <br /> This means an extra <b>$"+totalExtra+"</b> per pay or approximately an extra <b>$"+totalExtraInYear+"</b> over the rest of the year!"
-				} else {
-					response += "<br/><br/>";
-					if (!dayCpp && !dayEI) {
-						response += "It looks like you might not have enough pay periods to max out CPP nor EI contributions.";
-					} else if (!dayCpp) {
-						response += "It looks like you might not have enough pay periods to max out CPP contributions. Very very roughly your EI will max out on " + dayEI.format("MMMM Do");
-					} else if (!dayEI) {
-						response += "It looks like you might not have enough pay periods to max out EI contributions. Very very roughly your EI will max out on " + dayCpp.format("MMMM Do");
-					}
-				}
 
 
+			$("#results").html("Your CPP contribution is $" +Math.round(amountPerPay*cppPerc) + " per pay and your EI contribution is $" + Math.round(amountPerPay*eiPerc) +". "+ 
+
+				"<br/><br/>You should max out your CPP Contributions on your " + nth(Math.ceil(resultCPP))+ " pay. "+
+				"<br/>You should max out your EI Contributions on your " + nth(Math.ceil(resultEI))+ " pay. "+
+
+				"<br/><br/>Roughly this means " +dayCpp + " for CPP and " + dayEI+ " for EI."
 
 
-				$("#results").html(response);
+				);
 		}
 
-			$('html,body').animate({
-				scrollTop: $("#results").offset().top
-			});
-			$("#results").css("visibility", "visible");
-
-			ga('send','event', 'submitClick',numPays,"Number of pays per year");
-
-
-		});
-
-		var seasonGroupTemplate = $('#seasonGroupTemplate').html();
-
-		var dateRangePickerOptions = {
-			autoApply: true,
-			startDate: moment('01-01-' + year, 'MM DD YYYY'),
-			endDate: moment('12-31-' + year, 'MM DD YYYY'),
-		};
-
-		function getInvalidateDateRanges () {
-			return getDateRanges().filter(function (range, i, array) {
-				return array.reduce(function (isOverlapping, otherRange, j) {
-					if (i === j) {
-						return false;
-					}
-					return range.overlaps(otherRange);
-				}, false);
-			});
-		};
-
-		function warnInvalidDateRanges () {
-			$("#results").addClass("alert alert-danger");
-			$("#results").html("Your work periods are overlapping");
+			$(window).scrollTop($('#results').offset().top);
 			$("#results").css("visibility","visible");
-		}
 
-		function validateDateRanges () {
-			var isValid = !getInvalidateDateRanges().length;
-			if (!isValid) {
-				warnInvalidDateRanges();
-			}
-			return isValid;
-		}
+			ga('send','event', 'submitClick',numPays,"Number of pay periods per year");
 
-		function initDateRange ($input) {
-			$input.daterangepicker(dateRangePickerOptions);
-			$input.on('change', function (e) {
-				validateDateRanges();
-			})
-		}
 
-		initDateRange($('.date-range'));
-
-		$('.add-season-btn').click(function () {
-			if (!validateDateRanges()) {
-				return;
-			}
-			var seasonFragment = $(seasonGroupTemplate);
-			initDateRange(seasonFragment.find('.date-range'))
-			$('.seasonal-options').append(seasonFragment);
-			return false;
 		});
-
-		$('.seasonal-options').on('click', '.remove-season-btn', function (e) {
-			$(e.target).closest('.seasons-group').remove();
-		});
+		
 		
 
 		function nth(d) {
