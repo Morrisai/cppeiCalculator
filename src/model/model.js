1bf00bcb3be8fb350b7dc208a75143cf9838dab3
Aaron Morris <morrisa@gmail.com>
2023-01-28 15:13:43 -0500

    added build spec

--- src/model/model.js
+++ src/model/model.js
@@ -1,15 +1,15 @@
  /// DATA FROM: https://www.canada.ca/en/revenue-agency/services/tax/businesses/topics/payroll/payroll-deductions-contributions/canada-pension-plan-cpp/cpp-contribution-rates-maximums-exemptions.html
  // and from https://www.canada.ca/en/employment-social-development/programs/ei/ei-list/ei-employers/2019-maximum-insurable-earnings.html
  const Model = {
-    cppMaxEarning : 64900,
+    cppMaxEarning : 66600,
     cppExemptAmount : 3500,
-    cppPerc :  0.0570,
-    maxContribution: 3499.80,
+    cppPerc :  0.0595,
+    maxContribution: 3754.45,
 
 
-    eiMaxEarning : 60300,    
-    eiPerc : 0.0158,
-    year: 2022
+    eiMaxEarning : 61500,    
+    eiPerc : 0.0163,
+    year: 2023
 }
 
 export default Model
